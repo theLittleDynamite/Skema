4a945cebcd61fd1e909407bdecaf3de28a615faa
Amsterdam <jonobwilliams@hotmail.com>
2018-05-30 03:15:54 +1000

    many-to-one intitial test with source-point edge style

--- Skema/imports/ui/components/CytoscapeModel.js
+++ Skema/imports/ui/components/CytoscapeModel.js
@@ -8,32 +8,51 @@ export default class CytoscapeModel extends Component {
 			nodes: [
 				{ data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
 				{ data: { id: 'b' } },
-				{ data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
+				{ data: { id: 'c' }, position: { x: 300, y: 85 } },
 				{ data: { id: 'd' }, position: { x: 215, y: 175 } },
 				{ data: { id: 'e' } },
+                { data: { id: 'g' } },
 				{ data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
 			],
 			edges: [
 				{ data: { id: 'ad', source: 'a', target: 'd' } },
+                { data: { id: 'ac', source: 'a', target: 'c' } },
 				{ data: { id: 'eb', source: 'e', target: 'b' } }
-			]
+			],
+            style: [
+                {
+                    selector: 'edge',
+                    style: {
+                        'source-endpoint': ('180deg'),
+                        'line-color': 'red',
+                        'line-style': 'dashed'
+                    },
+                },
+                {
+                     selector: 'node',
+                     style: {
+                         'background-color': 'red'
+                     }
+                }
+            ]
 		};
 	}
-	
+
 	getContainerStyles() {
 		return {
+
 		};
 	}
 
 	render() {
 		return (
-			<ReactCytoscape 
+			<ReactCytoscape
 				containerID="cy"
 				elements={this.getElements()}
 				styleContainer={this.getContainerStyles()}
 				cyRef={(cy) => { this.cyRef(cy) }}
 				cytoscapeOptions={{ wheelSensitivity: 0.1 }}
-				layout={{ name: 'dagre' }} 
+				layout={{ name: 'dagre' }}
 			/>
 		);
 	}
