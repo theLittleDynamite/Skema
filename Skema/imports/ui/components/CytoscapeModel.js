bfa298f7d0912ad8286bf5c6f496a6ad40afeb84
Amsterdam <jonobwilliams@hotmail.com>
2018-05-31 16:27:01 +1000

    Style changes working, needs work on edge styles

--- Skema/imports/ui/components/CytoscapeModel.js
+++ Skema/imports/ui/components/CytoscapeModel.js
@@ -6,42 +6,64 @@ export default class CytoscapeModel extends Component {
 	getElements() {
 		return {
 			nodes: [
-				{ data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
+				{ data: { id: 'a' } },
 				{ data: { id: 'b' } },
-				{ data: { id: 'c' }, position: { x: 300, y: 85 } },
-				{ data: { id: 'd' }, position: { x: 215, y: 175 } },
+				{ data: { id: 'c' } },
+				{ data: { id: 'd' } },
 				{ data: { id: 'e' } },
                 { data: { id: 'g' } },
-				{ data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+				{ data: { id: 'f' } }
 			],
 			edges: [
 				{ data: { id: 'ad', source: 'a', target: 'd' } },
                 { data: { id: 'ac', source: 'a', target: 'c' } },
 				{ data: { id: 'eb', source: 'e', target: 'b' } }
-			],
-            style: [
-                {
-                    selector: 'edge',
-                    style: {
-                        'source-endpoint': ('180deg'),
-                        'line-color': 'red',
-                        'line-style': 'dashed'
-                    },
-                },
-                {
-                     selector: 'node',
-                     style: {
-                         'background-color': 'red'
-                     }
-                }
-            ]
+			]
 		};
 	}
 
-	getContainerStyles() {
-		return {
-
-		};
+	getStyles() {
+		return [{
+            selector: 'node',
+            css: {
+                'content': function content(ele) {
+                    return ele.data('label') || ele.data('id');
+                },
+                'text-valign': 'center',
+                'text-halign': 'center',
+                'background-color': 'red',
+                'background-opacity': 0.5
+            }
+        }, {
+            selector: '$node > node',
+            css: {
+                'padding-top': '10px',
+                'padding-left': '10px',
+                'padding-bottom': '10px',
+                'padding-right': '10px',
+                'text-valign': 'top',
+                'text-halign': 'center'
+            }
+        }, {
+            selector: 'edge',
+            css: {
+                'target-arrow-shape': 'circle',
+                'target-endpoint': 'outside-to-node'
+            }
+        }, {
+            selector: 'ad',
+            css: {
+                'line-color': 'blue'
+            }
+        }, {
+            selector: ':selected',
+            css: {
+                'background-color': 'black',
+                'line-color': 'black',
+                'target-arrow-color': 'black',
+                'source-arrow-color': 'black'
+            }
+        }];
 	}
 
 	render() {
@@ -49,7 +71,7 @@ export default class CytoscapeModel extends Component {
 			<ReactCytoscape
 				containerID="cy"
 				elements={this.getElements()}
-				styleContainer={this.getContainerStyles()}
+				style={this.getStyles()}
 				cyRef={(cy) => { this.cyRef(cy) }}
 				cytoscapeOptions={{ wheelSensitivity: 0.1 }}
 				layout={{ name: 'dagre' }}
