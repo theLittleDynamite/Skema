b4e58ad1bebf38af7f644f02e7e96c7d83ff9c53
Darian Brandt <brandt.darian@gmail.com>
2018-08-28 17:22:54 +1000

    SkemaV2 initial commit

---
+++ SkemaV2/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, () => {
+    console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
