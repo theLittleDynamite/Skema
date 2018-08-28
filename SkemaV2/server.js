1088656c2d867c6a4cd9411f8c02c54846709691
Darian Brandt <brandt.darian@gmail.com>
2018-08-28 17:55:56 +1000

    added basic mongoDB function

--- SkemaV2/server.js
+++ SkemaV2/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+
 
 app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('mongodb://Darian:Skema1@ds135852.mlab.com:35852/skema', (err, database) => {
+  // ... start the server
+})
 
 app.listen(3000, () => {
     console.log('listening on 3000')
