f433038ef854b9ce9c1f992120e57590c17182cb
Darian Brandt <brandt.darian@gmail.com>
2018-08-29 12:19:09 +1000

    Add items to MongoDB and view results via webpage

--- SkemaV2/server.js
+++ SkemaV2/server.js
@@ -1,23 +1,35 @@
-const express = require('express')
-const bodyParser= require('body-parser')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://cutlery:Skema1@ds135852.mlab.com:35852/skema';
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.urlencoded({extended: true}))
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err;
+    dbo = client.db('skema');
 
-MongoClient.connect('mongodb://Darian:Skema1@ds135852.mlab.com:35852/skema', (err, database) => {
-  // ... start the server
-})
-
-app.listen(3000, () => {
-    console.log('listening on 3000')
-})
+    // Listen for website requests on localhost
+    app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+    dbo.collection('nodes').find().toArray((err, results) => {
+        if (err) throw err;
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+        res.render('index.ejs', {nodes: results});
+    });
+});
+
+app.post('/nodes', (req, res) => {
+    dbo.collection('nodes').insertOne(req.body, (err, result) => {
+        if (err) throw err;
+
+        console.log('Saved to database.');
+        res.redirect('/');
+    });
+});
