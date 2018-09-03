f5dbd425e4718c7b7804a5c2eaa23d5ef8bfd09f
Darian Brandt <brandt.darian@gmail.com>
2018-09-03 23:18:12 +1000

    some comments and a basic readme.txt added

---
+++ SkemaV2/README.txt
@@ -0,0 +1,2 @@
+This program is built with node.js and express on the backend, using MongoDB as the 
+database and ejs as the templating engine to render specific pages.
--- SkemaV2/server.js
+++ SkemaV2/server.js
@@ -1,13 +1,22 @@
+// ================================================================
+// Get all the tools and variables we need
+// ================================================================
 var express = require('express');
 var bodyParser= require('body-parser');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://cutlery:Skema1@ds135852.mlab.com:35852/skema';
 
+// ================================================================
+// Setup our express application
+// ================================================================
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// ================================================================
+// Start server after we connect to our Mongo database
+// ================================================================
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     if (err) throw err;
     dbo = client.db('skema');
@@ -18,6 +27,10 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     });
 });
 
+// ================================================================
+// Display the main html page when a browser requests it.
+// The 'nodes' collection is retrieved from the db so it can be displayed.
+// ================================================================
 app.get('/', (req, res) => {
     dbo.collection('nodes').find().toArray((err, results) => {
         if (err) throw err;
@@ -26,6 +39,9 @@ app.get('/', (req, res) => {
     });
 });
 
+// ================================================================
+// Insert the html form data into the 'nodes' collection as one document.
+// ================================================================
 app.post('/nodes', (req, res) => {
     dbo.collection('nodes').insertOne(req.body, (err, result) => {
         if (err) throw err;
